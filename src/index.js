f6d8af66f2fe90a356b505d29fcc8dfc316b9ed7
Abhishek <abhishekkumar15238@gmail.com>
2025-05-03 13:59:05 +0530

    add methods in model

--- src/index.js
+++ src/index.js
@@ -7,7 +7,18 @@ import dotenv from "dotenv"
 dotenv.config({
   path:"./.env"
 })
-connectDB();
+connectDB().then(()=>{
+  app.on("error",(error)=>{
+    console.log("ERRR:",error);
+    throw error
+  })
+   app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is running at port: ${process.env.PORT}`);
+   })
+})
+.catch((err)=>{
+  console.log("MONGO db connection failed!!!",err);
+})
 /*(async()=>{
   try{
     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
